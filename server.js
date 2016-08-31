4310a1838d39a0e38350f9103d61576e34ec0ff9
bennycazares <benny.cazares@gmail.com>
2016-08-31 09:43:49 -0500

    added login routes

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ adminRouter.use(function(req, res, next) { // log each request to the console
         // continue doing what we were doing and go to the route
 next(); });
 
+// route middleware to validate :name
+ adminRouter.param('name', function(req, res, next, name) {
+ // do validation on name here
+ // blah blah validation
+ // log something so we know its working
+ console.log('doing name validations on ' + name);
+
+ // once validation is done save the new item in the req
+ req.name = name;
+ // go to the next thing
+ next();
+ });
+
+ // route with parameters (http://localhost:1337/admin/hello/:name)
+ adminRouter.get('/hello/:name', function(req, res) {
+ res.send('hello ' + req.name + '!');
+ });
+
  // users page (http://localhost:1337/admin/users)
  adminRouter.get('/users', function(req, res) {
    res.send('I show all the users!');
@@ -35,6 +53,17 @@ next(); });
 
  // apply the routes to our application
 app.use('/admin', adminRouter)
+
+app.route('/login')
+        // show the form (GET http://localhost:1337/login)
+.get(function(req, res) {
+res.send('this is the login form');
+})
+        // process the form (POST http://localhost:1337/login)
+.post(function(req, res) { console.log('processing');
+                res.send('processing the login form!');
+});
+
 
  // start the server
  app.listen(1337);
